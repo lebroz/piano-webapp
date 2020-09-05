82d812c71d01dbd3885d9dd58fee028d9ce0ecbd
romainlancia <romainlancia@gmail.com>
2020-09-05 15:16:51 +0200

    change sounds

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 const onMouseDown = id => {
     const el = document.getElementById(id);
+    if (!el) return;
     el.className = "key active";
     const sound = document.getElementById(`${id}-audio`);
+    if (!sound) return;
     sound.currentTime=0;
     sound.play();
 }
@@ -9,4 +11,5 @@ const onMouseDown = id => {
 const onMouseUp = id => {
     const el = document.getElementById(id);
     el.className = "key";
+    if (!el) return;
 }
