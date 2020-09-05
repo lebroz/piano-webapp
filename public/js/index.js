a914f7c84078442af2933f43fe1ac28e470a400b
romainlancia <romainlancia@gmail.com>
2020-09-05 14:46:38 +0200

    adding one more octave

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,9 @@ const onMouseDown = id => {
     const sound = document.getElementById(`${id}-audio`);
     sound.currentTime=0;
     sound.play();
-    console.log(el);
-    console.log(id);
 }
 
 const onMouseUp = id => {
     const el = document.getElementById(id);
     el.className = "key";
-    console.log(el);
-    console.log(id);
 }
